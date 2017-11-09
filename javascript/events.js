caf9b1675bd54bff2c14f235a5589c594ced2a04
Kevin Atkins <kaitoxas@gmail.com>
2017-11-09 07:10:52 -0700

    did a few events for the javascript assignment

--- javascript/events.js
+++ javascript/events.js
@@ -3,5 +3,9 @@ function mouseDown() {
 }
 
 function mouseUp() {
-	document.getElementById("event1").style.color = "black";
+	document.getElementById("event1").style.color = "blue";
+}
+
+function dblClick() {
+	document.getElementById("event2").innerHTML = "Captain, why are we out here chasing comets? This is not about revenge. This is about justice. Computer, lights up! Not if I weaken first. I will obey your orders. I will serve this ship as First Officer. And in an attack against the Enterprise, I will die with this crew. But I will not break my oath of loyalty to Starfleet. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
 }
